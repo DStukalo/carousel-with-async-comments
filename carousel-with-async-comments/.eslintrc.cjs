3b160e95efeaeed394f3490318a624d7aa3779de
DStukalo <94228608+DStukalo@users.noreply.github.com>
2023-06-29 14:49:41 +0300

    init: start project

---
+++ carousel-with-async-comments/.eslintrc.cjs
@@ -0,0 +1,103 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		jest: true,
+	},
+	extends: [
+		'airbnb',
+		'airbnb/hooks',
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended',
+		'./.eslintrc-typescript.cjs',
+		'plugin:react/jsx-runtime',
+	],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react'],
+	settings: {
+		'import/resolver': {
+			// typescript: {},
+			node: {
+				extensions: ['.js', '.jsx', '.ts', '.tsx'],
+			},
+		},
+	},
+	rules: {
+		'comma-dangle': [
+			'error',
+			{
+				objects: 'never',
+				arrays: 'never',
+				functions: 'never',
+				typeLiterals: 'ignore',
+				imports: 'never',
+				exports: 'never',
+			},
+		],
+		// 'comma-dangle': 0,
+		// '@typescript-eslint/comma-dangle': 0,
+		indent: [2, 'tab'],
+		'react/jsx-indent': [
+			2,
+			'tab',
+			{ checkAttributes: false, indentLogicalExpressions: true },
+		],
+		'react/jsx-indent-props': [2, 'tab'],
+		'no-tabs': 0,
+
+		'linebreak-style': [2, 'unix'],
+		quotes: [2, 'single'],
+		semi: [2, 'always'],
+		'no-plusplus': [2, { allowForLoopAfterthoughts: true }],
+		'react/jsx-filename-extension': [
+			2,
+			{
+				extensions: ['.js', 'jsx', '.ts', '.tsx'],
+			},
+		],
+		'import/prefer-default-export': 'off',
+		'import/extensions': [
+			2,
+			'ignorePackages',
+			{
+				js: 'never',
+				jsx: 'never',
+				ts: 'never',
+				tsx: 'never',
+			},
+		],
+		'react/require-default-props': ['off'],
+		'react/jsx-filename-extension': [
+			2,
+			{
+				extensions: ['.js', 'jsx', '.ts', '.tsx'],
+			},
+		],
+		'import/extensions': [
+			2,
+			'ignorePackages',
+			{
+				js: 'never',
+				jsx: 'never',
+				ts: 'never',
+				tsx: 'never',
+			},
+		],
+		'react/no-unknown-property': [2, { ignore: ['css'] }],
+		'react/jsx-props-no-spreading': 'off',
+		'no-param-reassign': [
+			'error',
+			{
+				props: true,
+				ignorePropertyModificationsFor: ['state'],
+			},
+		],
+	},
+};
