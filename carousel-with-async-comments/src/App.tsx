1652925ca18de2af0110ba20742ae1c7e035a89c
DStukalo <94228608+DStukalo@users.noreply.github.com>
2023-07-03 10:55:48 +0300

    refactor: update App component

--- carousel-with-async-comments/src/App.tsx
+++ carousel-with-async-comments/src/App.tsx
@@ -1,26 +1,25 @@
-import { Card } from 'flowbite-react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { MainPage } from './page/MainPage';
+import { Layout } from './components/Layout/Layout';
+import { NotFoundPage } from './page/404';
+import { PostPage } from './page/PostPage';
+
+export const routesConfig = [
+	{
+		path: '/',
+		element: <Layout />,
+		errorElement: <NotFoundPage />,
+		children: [
+			{ index: true, element: <MainPage /> },
+			{ path: '/post/:postID', element: <PostPage /> },
+		],
+	},
+];
+
+const router = createBrowserRouter(routesConfig);
 
 function App() {
-	return (
-		<article className="max-w-7xl h-screen flex justify-center items-center">
-			<div className="flex justify-center items-center flex-col">
-				<h1 className="text-2xl font-bold text-blue-600 mb-8 border-b-4 border-purple-600">
-					Test Vite + React + Flowbite
-				</h1>
-				<div className="max-w-sm">
-					<Card imgSrc="https://flowbite.com/docs/images/blog/image-1.jpg">
-						<h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-							Install & setup flowbite in react js
-						</h5>
-						<p className="font-normal text-gray-700 dark:text-gray-400">
-							Here are the biggest enterprise technology acquisitions of 2021 so
-							far, in reverse chronological order.
-						</p>
-					</Card>
-				</div>
-			</div>
-		</article>
-	);
+	return <RouterProvider router={router} />;
 }
 
 export default App;
